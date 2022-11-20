6823fafffa2a11907819c320754986c924e1b23b
LukaszSz90 <lukasz90sz@gmail.com>
2022-11-20 14:31:37 +0100

    frontend: add angular material

---
+++ src/app/shared/material.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatListModule} from '@angular/material/list';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatIconModule,
+    MatButtonModule
+  ],
+  exports: [
+    MatIconModule,
+    MatButtonModule,
+    MatListModule
+  ]
+})
+export class MaterialModule { }
