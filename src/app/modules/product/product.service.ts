fccec7a521cfbd98d87735d3eb99de79f46a5ddb
LukaszSz90 <lukasz90sz@gmail.com>
2022-11-20 16:40:56 +0100

    frontend: add product component

---
+++ src/app/modules/product/product.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { Product } from './model/product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  constructor() { }
+
+  getProducts(): Product[] {
+    return [
+      {
+        name: "Product 1",
+        category: "Kategoria 1",
+        description: "Opis productu 1",
+        price: 11.99,
+        currency: "PLN"
+      },
+      {
+        name: "Product 2",
+        category: "Kategoria 2",
+        description: "Opis productu 2",
+        price: 12.99,
+        currency: "PLN"
+      },
+      {
+        name: "Product 3",
+        category: "Kategoria 3",
+        description: "Opis productu 3",
+        price: 13.99,
+        currency: "PLN"
+      },
+      {
+        name: "Product 4",
+        category: "Kategoria 4",
+        description: "Opis productu 4",
+        price: 14.99,
+        currency: "PLN"
+      },
+      {
+        name: "Product 5",
+        category: "Kategoria 5",
+        description: "Opis productu 5",
+        price: 15.99,
+        currency: "PLN"
+      }
+    ];
+  }
+}
